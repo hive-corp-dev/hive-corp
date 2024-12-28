0d753bb0702b6232d7bba6b19579921a8f8aab06
Tatsuya <a.tatsu0117@gmail.com>
2024-12-29 00:40:51 +0900

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,21 @@ import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/blog/nextjs-14-app-router-google-analytics",
+        destination: "https://webtech-media.jp/article/nextjs-14-app-router-google-analytics",
+        permanent: true,
+      },
+      {
+        source: "/blog/nextjs-14-app-router-microsoft-clarity",
+        destination: "https://webtech-media.jp/article/nextjs-14-app-router-microsoft-clarity",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withContentCollections(nextConfig);
