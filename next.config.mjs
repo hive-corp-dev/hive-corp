5a955b9aecd64bdeab99ba640c00df06894fda41
Tatsuya <a.tatsu0117@gmail.com>
2025-01-04 16:29:30 +0900

    redirected css posts

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,16 @@ const nextConfig = {
         destination: "https://webtech-media.jp/article/nextjs-14-app-router-microsoft-clarity",
         permanent: true,
       },
+      {
+        source: "/blog/css-has-pseudo-class",
+        destination: "https://webtech-media.jp/article/css-has-pseudo-class",
+        permanent: true,
+      },
+      {
+        source: "/blog/css-not-pseudo-class",
+        destination: "https://webtech-media.jp/article/css-not-pseudo-class",
+        permanent: true,
+      },
     ];
   },
 };
